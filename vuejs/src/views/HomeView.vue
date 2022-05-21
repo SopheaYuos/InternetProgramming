cff3cf81b06267cf86a845594e30a39ad54bea36
sophea <dunno@gmail.com>
2022-05-21 14:32:53 +0700

    sophea

---
+++ vuejs/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script >
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  async mounted() {},
+  methods: {},
+};
+</script>
+
+<template>
+  <main>
+    <div class="bg-gray-100 py-2 text-center text-lg relative">
+      <img src="../assets/onetech.png" alt="" />
+      OneTech
+
+      <div
+        class="
+          absolute
+          text-white
+          right-2
+          top-2
+          bg-gray-400
+          px-2
+          rounded-md
+          cursor-pointer
+        "
+      >
+        <router-link to="/dashboard"><div class="">Dashboard</div></router-link>
+      </div>
+    </div>
+    <h1>You're in Home Page</h1>
+  </main>
+</template>
+
+<style>
+img {
+  width: 50px;
+}
+</style>
